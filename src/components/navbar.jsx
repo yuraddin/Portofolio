786593432d68072ed6a850124908abebb7f91655
Yuraddin <yuraddinaja@gmail.com>
2025-09-25 14:59:13 +0700

    perbaiki

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Impor Link dari react-router-dom
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   return (
